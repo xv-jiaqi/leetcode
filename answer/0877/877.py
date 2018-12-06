cac65d266d3497a7125e5f1cd14f21a35506ae96
linningmii <lin.zh.ming.27@gmail.com>
2018-12-06 22:39:23 +0800

    877.py

---
+++ answer/0877/877.py
@@ -0,0 +1,13 @@
+class Solution:
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+
+        return True
+
+
+s = Solution()
+
+print(s.stoneGame([5, 3, 4, 5]))  # True
