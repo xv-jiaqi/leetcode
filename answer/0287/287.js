852bf79bd1bad45fe223fc0906e1ca97d096be3b
linzhongming <lin.zh.ming.27@gmail.com>
2018-12-16 02:26:34 +0800

    287.js

--- answer/0287/287.js
+++ answer/0287/287.js
@@ -3,8 +3,15 @@
  * @return {number}
  */
 const findDuplicate = function (nums) {
+    // set的去重复杂度是
+    const set = new Set()
 
-
+    return nums.find(num => {
+        if (set.has(num)) {
+            return num
+        }
+        set.add(num)
+    })
 };
 
 console.log(findDuplicate([1, 3, 4, 2, 2]));
