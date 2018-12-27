48736005762113782756a371170798adb829066b
崆峒 <kongtong@wacai.com>
2018-12-28 02:00:22 +0800

    twoSum go

---
+++ answer/0001/01.go
@@ -0,0 +1,20 @@
+func twoSum(nums []int, target int) []int {
+  var m map[int]int
+  m = make(map[int]int)
+  var result []int
+
+  for i := 0; i < len(nums); i++ {
+    var complement int = target - nums[i]
+
+    c, ok := m [complement]
+    if (ok) {
+      result = append(result, c)
+      result = append(result, i)
+      break
+    }
+
+    m [nums[i]] = i
+    }
+    
+    return result
+}
